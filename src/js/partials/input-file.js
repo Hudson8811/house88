1c042cd904477548d719e2fd0967c02165f7b215
Troshin Sergey <serge-troshin@yandex.ru>
2021-07-09 10:34:11 +0300

    custom file

--- src/js/partials/input-file.js
+++ src/js/partials/input-file.js
@@ -9,43 +9,36 @@ function moreFiles(){
 
   if ($(this)[0].files && $(this)[0].files[0]) {
     counterFiles++;
-    inputer.closest('.custom-file').addClass('selected');
+    inputer.closest('.file-button').addClass('file-button--selected');
     
-    var white = '#fff';
-    if (inputer.parents('.brief__form').length > 0) white = '#000';
+   
 
-    var html = '<div class="custom-file">\n' +
-      '<input type="file" name="file[]" id="file'+counterFiles+'" class="inputfile js-new-file" />\n' +
-      '<label for="file'+counterFiles+'" class="btn">\n' +
-      '<svg version="1.1"xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"   viewBox="0 0 512 512"  xml:space="preserve">\n' +
-      '<g>\n' +
-      '<g>\n' +
-      '<path fill="'+white+'" d="M446.661,37.298c-49.731-49.731-130.641-49.731-180.372,0L76.378,227.208c-5.861,5.861-5.861,15.356,0,21.217\n' +
-      'c5.861,5.861,15.356,5.861,21.217,0l189.91-189.91c36.865-36.836,101.073-36.836,137.938,0c38.023,38.023,38.023,99.901,0,137.924\n' +
-      'l-265.184,268.17c-22.682,22.682-62.2,22.682-84.881,0c-23.4-23.4-23.4-61.467,0-84.867l254.576-257.577\n' +
-      'c8.498-8.498,23.326-8.498,31.825,0c8.776,8.776,8.776,23.063,0,31.84L117.826,400.958c-5.06,5.06-5.06,16.156,0,21.217\n' +
-      'c5.861,5.861,15.356,5.861,21.217,0l243.952-246.954c20.485-20.485,20.485-53.789,0-74.273c-19.839-19.839-54.449-19.81-74.258,0\n' +
-      'L54.161,358.524c-34.826,34.826-34.826,92.474,0,127.301C71.173,502.837,93.781,512,117.825,512s46.654-9.163,63.651-26.174\n' +
-      'L446.66,217.655C496.391,167.924,496.391,87.028,446.661,37.298z"/>\n' +
-      '</g>\n' +
-      '</g>\n' +
-      '</svg>\n' +
-      '<span>Прикрепить файл</span>\n' +
+    var html = '<div class="brief__form-file file-button custom-file">\n' +
+      '<input type="file" name="file[]" id="customFileBrief' + counterFiles + '" class="visually-hidden inputfile js-new-file" />\n' +
+      '<label for="customFileBrief' + counterFiles + '" class="btn">\n' +
+      '<span class="file-button__icon">\n' +
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="-96 0 512 512"><path d="M160 512C71.766 512 0 440.234 0 352V128c0-11.797 9.559-21.332 21.332-21.332 11.777 0 21.336 9.535 21.336 21.332v224c0 64.684 52.629 117.332 117.332 117.332S277.332 416.684 277.332 352V117.332c0-41.172-33.492-74.664-74.664-74.664-41.176 0-74.668 33.492-74.668 74.664v213.336c0 17.64 14.355 32 32 32s32-14.36 32-32V128c0-11.797 9.559-21.332 21.332-21.332 11.777 0 21.336 9.535 21.336 21.332v202.668c0 41.172-33.496 74.664-74.668 74.664s-74.668-33.492-74.668-74.664V117.332C85.332 52.652 137.961 0 202.668 0 267.371 0 320 52.652 320 117.332V352c0 88.234-71.766 160-160 160zm0 0"/></svg>\n' +
+      '</span>\n' +
+      '<span class="file-button__text">Прикрепить файлы</span>\n' +
       '</label>\n' +
-      '<div class="js-remove-file"></div>\n' +
+      '<button class="file-button__remove js-remove-file" type="button">\n' +
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M35.4 32l19.9-19.9c1-1 1-2.4 0-3.4s-2.4-1-3.4 0L32 28.6 12 8.8c-.9-1-2.4-1-3.3 0s-1 2.4 0 3.4L28.6 32 8.7 51.9c-1 1-1 2.4 0 3.4.5.4 1 .7 1.7.7s1.2-.2 1.7-.7l20-19.9 20 19.8c.5.4 1.2.7 1.7.7s1.2-.2 1.7-.7c1-1 1-2.4 0-3.4L35.4 32z"/></svg>\n' +
+      '</button>\n' +
       '</div>';
+
       
-    inputer.closest('.custom-files').prepend(html);
+      
+    inputer.closest('.brief__form-custom').prepend(html);
     initFiles()
 
     $('.js-remove-file').on('click', function (event) {
       event.preventDefault();
-      $(this).closest('.custom-file').remove();
+      $(this).closest('.file-button').remove();
     });
 
     moreFiles();
 
-    $('.custom-file.selected label').on('click', function (event) {
+    $('.file-button--selected label').on('click', function (event) {
       event.preventDefault();
     });
   }
@@ -71,7 +64,7 @@ function initFiles() {
                 fileName = e.target.value.split( '\\' ).pop();
 
             if( fileName )
-                label.querySelector( 'span' ).innerHTML = fileName;
+                label.querySelector( 'span.file-button__text' ).innerHTML = fileName;
             else
                 label.innerHTML = labelVal;
         });
