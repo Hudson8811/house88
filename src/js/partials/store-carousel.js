e45302fe4470b5c70cf93dde4a58345c15505ca3
PipZip <37911683+PipZip@users.noreply.github.com>
2021-07-19 14:51:58 +0600

    fix to fix

--- src/js/partials/store-carousel.js
+++ src/js/partials/store-carousel.js
@@ -1,26 +1,5 @@
 $(window).on('load', () => {
-  let windowWidth = $(window).width();
-  let breakpoint = 768;
-  let carousel = null;
-  let isInit = false;
-  
-  if (windowWidth <= breakpoint) {
-    initCarousel();
-    isInit = true;
-  }
-
-  $(window).on('resize', function() {
-    windowWidth = $(window).width();
-    
-    if (windowWidth <= breakpoint && !isInit) {
-      initCarousel();
-      isInit = true;
-    } else if (windowWidth > breakpoint && isInit) {
-      isInit = false;
-      carousel.destroy();
-      carousel = null;
-    }
-  })
+  initCarousel();
 
   function initCarousel() {
     carousel = new Swiper('.__js_store-carousel', {
